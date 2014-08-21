9fa8ff33c5f917bc71d6045a0a766adfddbd9e40
remibantos <Pupux456>
2014-08-22 00:25:02 +0200

    revert async loading notifer

--- admin/src/main/webapp/js/app.js
+++ admin/src/main/webapp/js/app.js
@@ -15,61 +15,6 @@
 
     });
 
-    /*
-    app.factory('httpInterceptor', function ($q, $rootScope, $log) {
-
-        var numLoadings = 0;
-
-        return {
-            request: function (config) {
-
-                numLoadings++;
-
-                // Show loader
-                $rootScope.$broadcast("loader_show");
-                return config || $q.when(config)
-
-            },
-            response: function (response) {
-
-                if ((--numLoadings) === 0) {
-                    // Hide loader
-                    $rootScope.$broadcast("loader_hide");
-                }
-
-                return response || $q.when(response);
-
-            },
-            responseError: function (response) {
-
-                if (!(--numLoadings)) {
-                    // Hide loader
-                    $rootScope.$broadcast("loader_hide");
-                }
-
-                return $q.reject(response);
-            }
-        };
-    });
-
-    app.config(function ($httpProvider) {
-            $httpProvider.interceptors.push('httpInterceptor');
-    });
-
-    app.directive("loader", function ($rootScope) {
-            return function ($scope, element, attrs) {
-                $scope.$on("loader_show", function () {
-                    return element.show();
-                });
-                return $scope.$on("loader_hide", function () {
-                    return element.hide();
-                });
-            };
-        }
-    );
-    */
-
-
     app.controller('DatepickerDemoCtrl',function ($scope) {
 
         $scope.today = function() {
