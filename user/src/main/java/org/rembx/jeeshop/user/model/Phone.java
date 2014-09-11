891fe5f5e9ac0a798b4b34f757940d7057faa65c
remibantos <Pupux456>
2014-09-12 00:08:19 +0200

    admin - Fix phone number validation issue in user form

--- user/src/main/java/org/rembx/jeeshop/user/model/Phone.java
+++ user/src/main/java/org/rembx/jeeshop/user/model/Phone.java
@@ -14,7 +14,6 @@ import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@NotNull
 @Size(max = 15)
 @Constraint(validatedBy = {})
 @Pattern(regexp = "\\+?[0-9]+")
