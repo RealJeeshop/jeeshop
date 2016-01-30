a6c69af7c9435ff44db096bd084b981dc254cabd
remibantos <remi.bantos@gmail.com>
2016-01-30 18:22:11 +0100

    Mailer test fix for Travis-CI

--- common/src/test/java/org/rembx/jeeshop/mail/MailerIT.java
+++ common/src/test/java/org/rembx/jeeshop/mail/MailerIT.java
@@ -4,32 +4,38 @@ import com.dumbster.smtp.SimpleSmtpServer;
 import com.dumbster.smtp.SmtpMessage;
 import org.jboss.weld.environment.se.Weld;
 import org.jboss.weld.environment.se.WeldContainer;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 
 import javax.mail.MessagingException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.fail;
 
-public class MailerCT {
+public class MailerIT {
 
     protected static Weld weld;
     protected static WeldContainer container;
 
-    private static SimpleSmtpServer server;
+    private SimpleSmtpServer server;
 
     @BeforeClass
     public static void init() {
         weld = new Weld();
         container = weld.initialize();
-        server = SimpleSmtpServer.start(9999);
     }
 
     @AfterClass
     public static void close() {
         weld.shutdown();
+    }
+
+    @Before
+    public void setup() {
+        server = SimpleSmtpServer.start(2525);
+    }
+
+    @After
+    public void after(){
         server.stop();
     }
 
@@ -47,4 +53,17 @@ public class MailerCT {
         assertThat(((SmtpMessage) server.getReceivedEmail().next()).getBody()).contains("<h1>Hello</h1>");
         assertThat(((SmtpMessage) server.getReceivedEmail().next()).getHeaderValue("Subject")).isEqualTo("Test Subject");
     }
+
+    @Test
+    public void sendMail_shouldThrowConnectTimeoutEx_WhenNoSmtpServerAvailable(){
+        server.stop();
+        Mailer mailer = container.instance().select(Mailer.class).get();
+        try {
+            mailer.sendMail("Test Subject", "test@test.com", "<h1>Hello</h1>");
+            fail("should have thrown ex");
+        }catch (MessagingException e){
+            assertThat(e.getMessage()).startsWith("Couldn't connect to host");
+        }
+    }
+
 }
