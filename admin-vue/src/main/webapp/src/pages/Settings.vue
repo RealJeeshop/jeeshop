9f81d6af5ad90dbd8ede672f30c2170e182a32d8
Vincent Truchot <vincent.truchot@qalyo.com>
2020-10-13 12:39:17 +0200

    fixed standard pages display

--- admin-vue/src/main/webapp/src/pages/Settings.vue
+++ admin-vue/src/main/webapp/src/pages/Settings.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="content">
+    <div class="page-content column padded-xl">
         <h1>Settings</h1>
         <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin posuere erat dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus commodo vel diam sed porta. Proin ut porta tortor, sit amet sollicitudin leo. Quisque placerat quam ante. Vivamus commodo, tellus a sodales facilisis, nibh augue rhoncus ex, eget ultricies odio metus sit amet nunc. Suspendisse tincidunt malesuada maximus. Sed et placerat nisl. Mauris venenatis, velit sed aliquam rhoncus, erat enim hendrerit massa, sed placerat ante risus dignissim risus.
