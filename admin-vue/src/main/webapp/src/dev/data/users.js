8120a460ce4d3e3c702ed3d537e811077714ff05
rbantos <rbantos@egencia.com>
2020-10-14 00:04:28 +0200

    admin-vue #55- mock users and catalogs resources when running admin-vue in dev mode. Add missing asciidoc code snippets for users resource

---
+++ admin-vue/src/main/webapp/src/dev/data/users.js
@@ -0,0 +1,22 @@
+module.exports =
+  [
+    {
+      "id": 1,
+      "login": "admin@jeeshop.org",
+      "password": "DjYu7nlNFk6BdxO+LwxZJ3mBAfxgwytTS2cVRbmnIO8=",
+      "firstname": "gerald",
+      "lastname": "min",
+      "gender": "m.",
+      "phoneNumber": "",
+      "address": null,
+      "deliveryAddress": null,
+      "birthDate": 1403042400000,
+      "age": null,
+      "creationDate": 1405807200000,
+      "updateDate": null,
+      "disabled": null,
+      "activated": true,
+      "preferredLocale": null,
+      "newslettersSubscribed": null
+    }
+  ]
